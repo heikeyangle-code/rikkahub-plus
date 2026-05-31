488b268c9dad2f5c4a3bb959999f68884fd8c9be
heikeyangle <heikeyangle@gmail.com>
2026-06-01 04:29:12 +0800

    fix: missing wrapContentWidth import

--- app/src/main/java/me/rerere/rikkahub/ui/components/message/ChatMessage.kt
+++ app/src/main/java/me/rerere/rikkahub/ui/components/message/ChatMessage.kt
@@ -17,6 +17,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.Icon
