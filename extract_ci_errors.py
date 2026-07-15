addfa2cb77ab6e93025dc0be1b5d65afff4602b4
heikeyangle <heikeyangle@gmail.com>
2026-07-15 16:25:14 +0800

    chore: 移除误提交的 CI 日志

--- extract_ci_errors.py
+++
@@ -1,7 +0,0 @@
-import zipfile
-z = zipfile.ZipFile('ci_logs_7fa92c3.zip')
-content = z.read('build/28_Build.txt').decode('utf-8', errors='replace')
-for line in content.split('\n'):
-    if ' e: ' in line:
-        txt = line.split('Z ', 1)[1] if 'Z ' in line else line
-        print(txt[:600])
